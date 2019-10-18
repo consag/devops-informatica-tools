b77fa12126ac0cbf746fcf812d7782c022ac938a
Jac <jac@jacbeekers.nl>
2019-10-18 03:14:02 +0200

    txt doc includes imports

--- generate_docs.sh
+++ generate_docs.sh
@@ -1,6 +1,13 @@
 for f in *.py ; do
+  if [ $f == "setup.py" ] ; then
+     continue
+  fi
   module=$(echo "$f" | cut -d"." -f1)
   pydoc3 $module > docs/text/${module}.txt
+  echo "IMPORTS" >> docs/text/${module}.txt
+  while read -r line ; do
+     echo "    $line" >> docs/text/${module}.txt
+  done < <(grep -e "^import" -e "^from.*import" $f)
   pydoc3 -w $module 
   mv ${module}.html docs/html/
 done
