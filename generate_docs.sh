767af1fee3132c476cffaef61095dedfcdddab12
ABNJac <jac.beekers@nl.abnamro.com>
2020-03-26 21:10:37 +0100

    docs merged from Develop

--- generate_docs.sh
+++ generate_docs.sh
@@ -1,14 +1,16 @@
-for f in *.py ; do
-  if [ $f == "setup.py" ] ; then
+thisDir=$(pwd)
+for f in cicd/*.py execution/*.py supporting/*.py scripts/*.py ; do
+  if [ $(basename $f) == "setup.py" ] ; then
      continue
   fi
-  module=$(echo "$f" | cut -d"." -f1)
+  module=$(echo "$(basename $f)" | cut -d"." -f1)
+  dir=$(dirname $f)
   pydoc3 $module > docs/text/${module}.txt
   echo "IMPORTS" >> docs/text/${module}.txt
   while read -r line ; do
      echo "    $line" >> docs/text/${module}.txt
   done < <(grep -e "^import" -e "^from.*import" $f)
-  pydoc3 -w $module 
-  mv ${module}.html docs/html/
+  pydoc3 -w $f
+  mv ${module}.html ${thisDir}/docs/html/
 done
 
